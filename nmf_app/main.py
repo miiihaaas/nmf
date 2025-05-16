27d657f99ad428db6db2e748b6236b7e18b98080
Mihailo Panić <miiihaaas@gmail.com>
2025-05-16 22:19:50 +0200

    udate

--- nmf_app/main.py
+++ nmf_app/main.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from nmf_app.models import PaymentSlip
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from nmf_app.models import PaymentSlip, Payment
 
 main = Blueprint("main", __name__)
 
@@ -13,7 +13,22 @@ def payment_slips():
     payment_slips = PaymentSlip.query.all()
     return render_template("payment_slips.html", payment_slips=payment_slips)
 
-@main.route("/import_xml", methods=["GET", "POST"])
-def import_xml():
-    return render_template("import_xml.html")
+@main.route("/payments", methods=["GET", "POST"])
+def payments():
+    payments = Payment.query.all()
+    if request.method == "POST":
+        xml_file = request.files["xmlFile"]
+        if xml_file and xml_file.filename.endswith(".xml"):
+            # Obradi XML fajl
+            flash("Uspešno je učitan XML fajl.", "success")
+            return redirect(url_for("main.payments"))
+        else:
+            flash("Nije pronađen fajl sa ekstenzijom .xml.", "error")
+    return render_template("payments.html", payments=payments)
+
+@main.route("/edit_payment/<int:payment_id>", methods=["GET", "POST"])
+def edit_payment(payment_id):
+    payment = Payment.query.get_or_404(payment_id)
+    return render_template("edit_payment.html", payment=payment)
+
 
